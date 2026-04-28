4f947101917d66dcc05aac1abf5f23eb90d4c518
Emma Shen <emmaxshen@berkeley.edu>
2026-04-27 21:24:50 -0700

    first commit!

---
+++ FirstAidKit/FirstAidKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirstAidKit
+//
+//  Created by Emma Shen on 4/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello first commit!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirstAidKit/FirstAidKit/FirstAidKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAidKitApp.swift
+//  FirstAidKit
+//
+//  Created by Emma Shen on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct FirstAidKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
